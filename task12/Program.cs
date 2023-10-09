91c4d17d9fe81a1b3824457a47392698d6ad430d
AlekseyGardevuar <aleksey.konashenkov1999@mail.ru>
2023-10-09 14:46:29 +0300

    Прошел второй семинар

---
+++ task12/Program.cs
@@ -0,0 +1,20 @@
+﻿/*12.Напишите программу, которая будет принимать на вход два числа и выводить, является ли 
+второе число кратным первому. 
+Если второе число некратно первому, то программа выводит остаток от деления.
+34, 5 -> некратно, остаток 4
+16, 4 -> кратно
+*/
+
+System.Console.Write("Задай первое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Задай первое число:");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+if(numberTwo % number == 0)
+{
+    System.Console.WriteLine($"Число {number} кратное числу {numberTwo}!");
+}
+else
+{
+    int result = numberTwo % number;
+    System.Console.WriteLine($"Остаток от деления: {result}");
+}
---
+++ task16/Program.cs
@@ -0,0 +1,22 @@
+﻿/*16.Напишите программу, которая принимает на вход два числа и проверяет, 
+является ли одно число квадратом другого.
+5, 25 -> да
+-4, 16 -> да
+25, 5 -> да
+8,9 -> нет
+*/
+
+System.Console.Write("Задай первое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Задай первое число:");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+int kvadrat1 = number * number;
+int kvadrat2 = numberTwo * numberTwo;
+if(numberTwo % kvadrat1 == 0 || number % kvadrat2 == 0)
+{
+    System.Console.WriteLine("Условие задачи выполняется!");
+}
+else
+{
+    System.Console.WriteLine("Условие задачи не выполняется!");
+}
